4f0789efa05c438b117535f8c317d4b2f2a7f418
dim3003 <dimitri.andre@unil.ch>
2021-10-10 15:19:16 +0200

    marketcap+refactor

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,43 @@
 #Basic Utilities
 import pandas
+from datetime import date, timedelta
+
 #Data fetchers
 import san
 
+#################
+#Functions
+################
 
-cryptoName = san.get("projects/all").name #finds all crypto names
+def getMarketcap(crypto="bitcoin"):
+    mktcap = san.get(
+        f"marketcap_usd/{crypto}",
+        from_date=today,
+        to_date=today
+        )
+    return mktcap.value[-1]
+
+
+#################
+#Script
+#################
+
+#finds all crypto names
+#cryptoName = san.get("projects/all").name
+
+#get date
+today_raw = date.today()
+today = today_raw.strftime("%Y-%m-%d")
+
+
+
+"""
+bite = san.get(
+    "ohlc/santiment",
+    from_date="2012-06-01",
+    to_date="2020-06-05",
+    interval="1d"
+)
+
+print(len(bite))
+"""
