c683d3104d213eeffed6d06f80d48c4f1f4ff8ab
Guillaume Pavé <guillaumepave@gmail.com>
2021-10-18 12:06:05 +0200

    script to create index

--- detect_first_date.py
+++ detect_first_date.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 ## create file to know first date of crypto ##
 crypto = []
 first_dates = []
-files = os.listdir('./data/')
+files = os.listdir('./data/raw/')
 
 is_create_file = input('Do you want to create a new file date ? [y/no]')
 
@@ -15,7 +15,7 @@ def create_file():
         try:
             
             #print(f.split(".")[0])
-            df = pd.read_pickle(f"data/{f}")
+            df = pd.read_pickle(f"./data/raw/{f}")
             #print(df.head(5))
             date = datetime.date(df.index[0])
             first_dates.append(date)
@@ -29,12 +29,12 @@ def create_file():
     }
 
     df = pd.DataFrame(data)
-    df.to_csv('./first_date_crypto.csv', index=False)
+    df.to_csv('./data/processed/first_date_crypto.csv', index=False)
 
 if is_create_file == 'y':
     create_file()
 
-df = pd.read_csv('./first_date_crypto.csv')
+df = pd.read_csv('./data/processed/first_date_crypto.csv')
 
 bitcoin = df[(df['crypto_name'] == 'bitcoin')]
 bitcoin_date = bitcoin['first_date']
@@ -42,7 +42,7 @@ bitcoin_date = bitcoin['first_date']
 #modif here to first newvalue
 # create loop inside a with file to find the best matching with first date after bitcoin
 other_crypto = df[(df['first_date'] == bitcoin_date.values[0])]
-other_crypto.to_csv('./crypto_begin_with_bitcoin.csv')
+other_crypto.to_csv('./data/processed/crypto_begin_with_bitcoin.csv')
 print(bitcoin)
 print(bitcoin_date.values[0])
 print(other_crypto.head(5))
