caca5dc476840462d49f0a4ff4815c699d68f742
dim3003 <dimitri.andre@unil.ch>
2021-11-20 21:39:36 +0100

    fix ew mv missing reb

--- strats/EW.py
+++ strats/EW.py
@@ -47,7 +47,7 @@ results_7 = createPortfolio7(df_weights, df_returns)
 df_price_7 = results_7[0]
 turnover_monthly_7 = results_7[1]
 df_metrics_7 = pd.read_csv(f"../data/processed/df_metrics_{c.number_cryptos}_1e{marketcap[-1]}_reb7.csv", index_col=0)
-df_metrics_7.loc["CW", "monthly_turnover"] = turnover_monthly_7
+df_metrics_7.loc["EW", "monthly_turnover"] = turnover_monthly_7
 df_metrics_7.to_csv(f"../data/processed/df_metrics_{c.number_cryptos}_1e{marketcap[-1]}_reb7.csv")
 df_price_7.to_csv(f"../data/strats/EW_price_{c.number_cryptos}_1e{marketcap[-1]}_reb7.csv")
 
@@ -56,6 +56,6 @@ results_30 = createPortfolio30(df_weights, df_returns)
 df_price_30 = results_30[0]
 turnover_monthly_30 = results_30[1]
 df_metrics_30 = pd.read_csv(f"../data/processed/df_metrics_{c.number_cryptos}_1e{marketcap[-1]}_reb30.csv", index_col=0)
-df_metrics_30.loc["CW", "monthly_turnover"] = turnover_monthly_30
+df_metrics_30.loc["EW", "monthly_turnover"] = turnover_monthly_30
 df_metrics_30.to_csv(f"../data/processed/df_metrics_{c.number_cryptos}_1e{marketcap[-1]}_reb30.csv")
 df_price_30.to_csv(f"../data/strats/EW_price_{c.number_cryptos}_1e{marketcap[-1]}_reb30.csv")
